8d69ede322ff9a503fb932e80674cbe022709776
JoelDamian <joel.damian.echeverria@gmail.com>
2023-11-26 12:48:50 -0400

    Exercise 1.10

--- part1/unicafe/src/components/statistics/Statistics.js
+++ part1/unicafe/src/components/statistics/Statistics.js
@@ -1,31 +1,34 @@
 import React from 'react';
-import Title from '../title/Title';
+import StatisticLine from '../statistic-line/StatisticLine';
 
 const Statistics = ({ good, neutral, bad }) => {
+  const getAll = () => good + neutral + bad;
 
-    const getAll = () => good + neutral + bad;
-
-    const positivePercentage = () => getAll() === 0 ? 0 : ((good * 100) / getAll());
+  const positivePercentage = () =>
+    getAll() === 0 ? 0 : ((good * 100) / getAll()).toFixed(1);
 
   const average = () => {
     const totalScore = good - bad;
-    return getAll() === 0 ? 0 : (totalScore / getAll());
+    return getAll() === 0 ? 0 : (totalScore / getAll()).toFixed(1);
   };
 
-  if(getAll() !== 0){
+  if (getAll() !== 0) {
     return (
-        <div>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {getAll()}</p>
-          <p>average {average()}</p>
-          <p>positive {positivePercentage()}%</p>
-        </div>
-      );
+      <div>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={getAll()} />
+        <StatisticLine text='average' value={average()} />
+        <StatisticLine
+          text='positive'
+          value={positivePercentage().toString() + '%'}
+        />
+      </div>
+    );
   }
 
-  return <p>No feedback given</p>
+  return <p>No feedback given</p>;
 };
 
 export default Statistics;
