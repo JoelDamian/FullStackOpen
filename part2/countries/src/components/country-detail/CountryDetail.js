b9e1b721a00917a8648034e51bae237a11627f90
JoelDamian <joel.damian.echeverria@gmail.com>
2023-11-27 09:55:00 -0400

    Exercise 2.14

--- part2/countries/src/components/country-detail/CountryDetail.js
+++ part2/countries/src/components/country-detail/CountryDetail.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './CountryDetail.css';
+import axios from 'axios';
 
-const CountryDetail = ({country}) => {
+const api_key = process.env.REACT_APP_API_KEY;
 
-    const keys = Object.keys(country.languages);
+const CountryDetail = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+  const keys = Object.keys(country.languages);
 
-    return(<div>
-        <h2>{country.name.common}</h2>
-        <div className="capital-container">
-            capital:{' '}
-            {country.capital !== undefined && country.capital.map((capital, index) => (
-                <p key={index}>{capital}{' '}</p>
-            ))}
+  const initialize = () => {
+    if (country.capital !== undefined) {
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital[0]}`
+        )
+        .then((response) => {
+          setWeather(response.data);
+        })
+        .catch((error) => console.error(error));
+    } else {
+      setWeather(null);
+    }
+  };
+
+  useEffect(initialize, [country.capital]);
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div className='capital-container'>
+        capital:{' '}
+        {country.capital !== undefined &&
+          country.capital.map((capital, index) => (
+            <p key={index}>{capital} </p>
+          ))}
+      </div>
+      <p>populaton: {country.population}</p>
+      <h3>Spoken languages</h3>
+      <ul>
+        {keys.map((key, index) => (
+          <li key={index}>{country.languages[key]}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt='flag' />
+
+      {weather !== null && (
+        <div>
+          <h3>Weather in {country.capital[0]}</h3>
+          <p><strong>temperature:</strong>{' '}{weather.current.temperature}{' '}Celcius</p>
+          <img src={weather.current.weather_icons[0]} alt='weather' />
+          <p><strong>wind:</strong>{' '}{weather.current.wind_speed}{' '}mph direction {weather.current.wind_dir}</p>
         </div>
-        <p>populaton: {country.population}</p>
-        <h3>languages</h3>
-        <ul>
-            {keys.map((key, index) => (
-                <li key={index}>{country.languages[key]}</li>
-            ))}
-        </ul>
-        <img src={country.flags.png} alt="flag"/>
-    </div>)
+      )}
+    </div>
+  );
 };
 
 export default CountryDetail;
