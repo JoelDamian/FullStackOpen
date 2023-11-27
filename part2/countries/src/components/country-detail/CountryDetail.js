b5cbc0e16d2c942d5fd432402e605e31e4433117
JoelDamian <joel.damian.echeverria@gmail.com>
2023-11-27 08:41:33 -0400

    Exercise 2.13

--- part2/countries/src/components/country-detail/CountryDetail.js
+++ part2/countries/src/components/country-detail/CountryDetail.js
@@ -9,15 +9,15 @@ const CountryDetail = ({country}) => {
         <h2>{country.name.common}</h2>
         <div className="capital-container">
             capital:{' '}
-            {country.capital.map((capital) => (
-                <p>{capital}{' '}</p>
+            {country.capital !== undefined && country.capital.map((capital, index) => (
+                <p key={index}>{capital}{' '}</p>
             ))}
         </div>
         <p>populaton: {country.population}</p>
         <h3>languages</h3>
         <ul>
-            {keys.map((key) => (
-                <li>{country.languages[key]}</li>
+            {keys.map((key, index) => (
+                <li key={index}>{country.languages[key]}</li>
             ))}
         </ul>
         <img src={country.flags.png} alt="flag"/>
--- part2/countries/src/components/country-list/CountryList.js
+++ part2/countries/src/components/country-list/CountryList.js
@@ -1,21 +1,38 @@
-import React from "react";
-import CountryDetail from "../country-detail/CountryDetail";
+import React, { useState, useEffect } from 'react';
+import CountryDetail from '../country-detail/CountryDetail';
+import './CountryList.css';
 
-const CountryList = ({countries}) => {
+const CountryList = ({ countries }) => {
+    const [conutryList, setConutryList] = useState([]);
 
-    if(countries.length > 10){
-        return <p>Too many matches, specify another filter</p>
+    useEffect(() => {
+        setConutryList(countries);
+    },[countries]);
+
+    const getConutry = (country) => {
+        const newCountryList = [country];
+        setConutryList(newCountryList)
     }
 
-    if(countries.length === 1){
-        return <CountryDetail country={countries[0]}/>
-    }
-    
-    return(<div>
-        {countries.map((country) => (
-            <p key={country.name.common}>{country.name.common}</p>
-        ))}
-    </div>)
+  if (conutryList.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
+
+  if (conutryList.length === 1) {
+    return <CountryDetail country={conutryList[0]} />;
+  }
+
+
+  return (
+    <div>
+      {conutryList.map((country) => (
+        <div key={country.name.common} className='country-container'>
+          <p >{country.name.common}</p>
+          <button onClick={() => getConutry(country)}>show</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CountryList;
